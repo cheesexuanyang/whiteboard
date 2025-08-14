3834f9aa9d41021a9b3fdceed00b9da17044192f
cheesexuanyang <cheexuanyang@gmail.com>
2025-08-15 02:20:13 +0800

    sizepicker and toolbar

---
+++ frontend/src/components/sizepicker/sizepicker.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import './SizePicker.css';
+
+function SizePicker({ brushSize, setBrushSize }) {
+  const sizePresets = [2, 5, 10, 15, 20];
+
+  return (
+    <div className="size-section">
+      <span className="size-label">Size:</span>
+      {sizePresets.map((size) => (
+        <button
+          key={size}
+          onClick={() => setBrushSize(size)}
+          className={`size-button ${brushSize === size ? 'active' : ''}`}
+        >
+          {size}
+        </button>
+      ))}
+    </div>
+  );
+}
+
+export default SizePicker;
